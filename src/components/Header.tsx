16f5ab52bdb28f9524c627cd62c763e45881782e
RAHUL SAINI <98648234+ralogic@users.noreply.github.com>
2026-03-27 11:09:47 +0530

    dfdf

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,19 +13,6 @@ export default function Header() {
           </Link>
 
         </div>
-        <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-        <script>
-          window.OneSignalDeferred = window.OneSignalDeferred || [];
-          OneSignalDeferred.push(async function(OneSignal) {
-            await OneSignal.init({
-              appId: "ef0bd327-e01f-44ef-a0e1-0b80f77d28c4",
-              safari_web_id: "web.onesignal.auto.10bba952-d3e6-4be7-b269-bd5caae877a4",
-              notifyButton: {
-                enable: true,
-              },
-            });
-       });
-        </script>
         <div className="nav-links flex items-center gap-4">
           <Link href="/tools" className="text-slate-700 transition-colors hover:text-blue-600 dark:text-slate-300 dark:hover:text-blue-400">
             Tools
